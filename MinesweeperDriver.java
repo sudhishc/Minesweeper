38d488ccc1826db2822dd9311aa5e1943863b3b7
sudhishc <41487454+sudhishc@users.noreply.github.com>
2018-07-23 09:18:35 -0400

    Update MinesweeperDriver.java

--- MinesweeperDriver.java
+++ MinesweeperDriver.java
@@ -7,11 +7,25 @@ public class MinesweeperDriver
 {
    public static void main(String[] args)
    {
+      setLayout(new BorderLayout());
+      
       JFrame frame = new JFrame("Final Project: Minesweeper!");
       JPanel panel = new JPanel();
       frame.setSize(1600, 1600);
       frame.setLocation(0, 0);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      JPanel panel = new JPanel();
+      add(panel);
+      
+      JLabel menu = new Jlabel("Menu");
+      add(menu, BorderLayout.NORTH);
+      
+      JButton beginner = new JButton("Beginner");
+      
+      
+      
+      
       frame.setContentPane(new Minesweeper());
       frame.setVisible(true);
    }
